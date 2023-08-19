5ee83e670fe1f97718bb78669dcdc25f6f6b4baf
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-08-20 00:15:51 +0300

    image-finder

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,5 @@
 import { Btn } from './Button.styled';
 
 export const Button = ({ onClick }) => {
-  return (
-    <Btn type="button" onClick={onClick}>
-      Load more
-    </Btn>
-  );
+  return <Btn onClick={onClick}>Load more</Btn>;
 };
